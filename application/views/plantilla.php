5cb784fbed2d2aaa11d01381d2b95a456bd61925
AgustRuiz <agustinruizlinares@gmail.com>
2014-07-17 18:43:49 +0200

    Corregidos ficheros SVG para hacerlos trazado en lugar de texto

--- application/views/plantilla.php
+++ application/views/plantilla.php
@@ -5,7 +5,7 @@
     <!-- <p class="pull-right visible-xs"><button type="button" class="btn btn-primary btn-xs" data-toggle="offcanvas">Toggle nav</button></p> -->
     <div class="jumbotron">
     	<!-- Esto es una chapuza, pero es provisional -->
-    	<center><img src="<?=$this->config->base_url();?>/img/logo/logo.svg" alt="crm-star" style="width:50%;"></center>
+    	<center><img src="<?=$this->config->base_url();?>/img/logo/logo.svg" alt="crm-star" style="width:60%;"></center>
     	<p class="text-center">Client Relationship Management Star</p>
    </div>
 
