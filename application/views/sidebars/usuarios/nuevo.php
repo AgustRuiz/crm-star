c2a4b98a2c3fa98a849e1c8c6c708029c7347236
AgustRuiz <agustinruizlinares@gmail.com>
2014-08-08 23:27:32 +0200

    Corregido menú de nuevo Usuario (mensaje de eliminar)

--- application/views/sidebars/usuarios/nuevo.php
+++ application/views/sidebars/usuarios/nuevo.php
@@ -3,7 +3,7 @@
 		<a href="<?=$this->config->base_url()?>usuarios" class="list-group-item">Listar usuarios</a>
 		<a href="#" class="list-group-item active">Nuevo usuario</a>
 		<a href="#" class="list-group-item disabled">Editar usuario</a>
-		<a href="#" class="list-group-item disabled">¿Eliminar usuario?</a>
+		<a href="#" class="list-group-item disabled">Eliminar usuario</a>
 	</div>
 </div><!--/span-->
 </div><!--/row-->
